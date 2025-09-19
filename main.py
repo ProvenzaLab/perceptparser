3913dbfc9f8d6f8f131b69fd6b5daace9366053b
timonmerk <timon.merk95@gmail.com>
2025-09-19 11:28:46 -0500

    restructure repo

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from percept import PerceptParser
+from percept_parser import PerceptParser
 import os
 
-filename = "Report_Json_Session_Report_20250425T080622.json"
+filename = "Report_Json_Session_Report_20250410T170448.json"
+filename = "/Users/Timon/Downloads/Report_Json_Session_Report_20250626T181946.json"
+filename = '/Users/Timon/Downloads/Report_Json_Session_Report_20250709T102453 (1).json'
 
 parser = PerceptParser(filename)
 dir_name = os.path.basename(filename)[:-len(".json")]
