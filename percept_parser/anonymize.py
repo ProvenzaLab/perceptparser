e1ebcd67a0641bcd1a5510006996df0b1cbaeeed
TomekFraczek <tfraczek@uw.edu>
2026-03-13 14:12:53 -0500

    Added some better verbosity control

--- percept_parser/anonymize.py
+++ percept_parser/anonymize.py
@@ -27,42 +27,48 @@ def deep_update(full_key, new_value, json_data, verbose=True):
         deep_update(full_key[1:], new_value, json_data[full_key[0]])
 
 
-def remove_serial(json_data):
+def remove_serial(json_data, verbose=True):
     """Function to search for and remove device serial numbers"""
     for full_key, value in recurse(json_data):
         if 'serialnumber' in full_key[-1].lower():
-            deep_update(full_key, "##########", json_data)
+            deep_update(full_key, "##########", json_data, verbose=verbose)
 
 
-def remove_patient_info(json_data):
+def remove_patient_info(json_data, verbose=True):
     """Function to search for and remove a patient name"""
     name_re = re.compile(r'[pP]atient.*[nN]ame')
     for full_key, value in recurse(json_data):
         if name_re.match(full_key[-1]):
-            deep_update(full_key, "REMOVED", json_data)
+            deep_update(full_key, "REMOVED", json_data, verbose=verbose)
 
 
-def obfuscate_dates(json_data):
+def obfuscate_dates(json_data, verbose=True):
     """Function to obfuscate all other dates by adding a random shift"""
     date_shift = pd.Timedelta(seconds=random.randint(-10**8, 10**8))
     date_search = re.compile(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z')
     for full_key, value in recurse(json_data):
         if isinstance(value, str) and date_search.search(value):
             new_datetime = (pd.to_datetime(value) + date_shift).strftime('%Y-%m-%dT%H:%M:%SZ')
-            deep_update(full_key, new_datetime, json_data)
+            deep_update(full_key, new_datetime, json_data, verbose=verbose)
 
 
-def anonymize(json_filepath, output=None, serial=True, name=True, dates=True):
+def anonymize(json_filepath, output=None, serial=True, name=True, dates=True, verbose=True):
     """Function to anonymize a JSON object"""
     with open(json_filepath, 'r') as file:
         json_data = json.load(file)
 
     if serial:
-        remove_serial(json_data)
+        if verbose:
+            print(f'Removing device serial info')
+        remove_serial(json_data, verbose=verbose)
     if name:
-        remove_patient_info(json_data)
+        if verbose:
+            print(f'Removing patient name info')
+        remove_patient_info(json_data, verbose=verbose)
     if dates:
-        obfuscate_dates(json_data)
+        if verbose:
+            print(f'Obfuscating all dates')
+        obfuscate_dates(json_data, verbose=verbose)
 
     output = json_filepath if output is None else output
     with open(output, 'w') as file:
@@ -75,7 +81,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("json_filepath", help="Path to the JSON file to anonymize")
     parser.add_argument("--output", help="Path to save the anonymized JSON file")
+    parser.add_argument("-v", "--verbose", help="Verbose output", action="store_true")
 
     args = parser.parse_args()
 
-    anonymize(args.json_filepath, output=args.output)
+    anonymize(args.json_filepath, output=args.output, verbose=args.verbose)
